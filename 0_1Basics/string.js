8026256c5173eb2d9b6bda87967141f51bd557c6
shivalika <shivalika.manu@gmail.com>
2024-01-04 07:38:36 +0530

    string added

--- 0_1Basics/string.js
+++ 0_1Basics/string.js
@@ -7,3 +7,12 @@ console.log(gm.toUpperCase)
 console.log(gm.toUpperCase())
 console.log(gm.charAt(2));
 console.log(gm.indexOf('o'))
+//trim substring,slice,replace,includes,split
+let name1 = "                sjovlaika        "
+console.log(name1)
+console.log(name1.trim())
+console.log(name1.trimEnd())
+console.log(name1.trimStart())
+//substring
+let x=name1.substr(0,4) //it will go till endca
+console.log(x) 
